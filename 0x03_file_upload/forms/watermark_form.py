0c8b8dd8f317bcfb11670e30ade8eb073777c251
anjing <anjingcuc@gmail.com>
2017-10-09 09:24:58 +0800

    增加文件上传一节。

---
+++ 0x03_file_upload/forms/watermark_form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileRequired
+
+
+class WatermarkForm(FlaskForm):
+    watermark = StringField('watermark', validators=[DataRequired()])
+    image = FileField('image', validators=[FileRequired()])
